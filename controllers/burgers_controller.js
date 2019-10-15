be1e3972155495cfeda0df8a48dcb65aa532be75
Mike Prap <michaelpraplaski@yahoo.com>
2019-10-14 21:17:04 -0400

    added an update in controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,3 +15,19 @@ router.get("/", function(req, res) {
       res.render("index", hbsObject);
     });
   });
+  outer.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
